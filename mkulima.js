20510c5b1d4ddccdadd9f8d528122b76e69b718c
gathangamaryann <maryanngathanga@gmail.com>
2022-08-03 11:10:34 +0300

    initialized arguments

--- mkulima.js
+++ mkulima.js
@@ -1,5 +1,5 @@
 class Mkulima{
-    constructor(){
+    constructor(farms,products){
     this.farms = [];
     this.groceryVendors = [];
     this.products = [];
@@ -7,17 +7,17 @@ class Mkulima{
     this.farms.push({farmId,farmName,farmer,phone,address})
     }
     this.removeFarm = (farmId) => {
-    let specific = this.farms.find(item => item.farmId ===farmId);
-    let farmIndex = this.farms.indexOf(specific);
+    let farm = this.farms.find(item => item.farmId ===farmId);
+    let farmIndex = this.farms.indexOf(farm);
     this.farms.splice(farmIndex, 1);
     }
     this.updateFarm = function(farmId,newFarmId,newFarmName,newFarmersName,newPhone,newAddress){
-    let specificFarm = this.farms.find(item => item.farmId ===farmId)
-    specificFarm.farmId =newFarmId;
-    specificFarm.farmName =newFarmName;
-    specificFarm.farmer =newFarmersName;
-    specificFarm.phone =newPhone;
-    specificFarm.address =newAddress;
+    let theFarm = this.farms.find(item => item.farmId ===farmId)
+    theFarm.farmId =newFarmId;
+    theFarm.farmName =newFarmName;
+    theFarm.farmer =newFarmersName;
+    theFarm.phone =newPhone;
+    theFarm.address =newAddress;
     }
     this.getFarm = (Id)=>{
     console.log(this.farms.find(object=>object.farmId===Id))
@@ -26,15 +26,15 @@ class Mkulima{
     this.product.push({productId,productName,price})
     }
     this.removeProduct = (productId) => {
-    let specific = this.product.find(item => item.productId ===productId);
+    let farm = this.product.find(item => item.productId ===productId);
     let productIndex = this.product.indexOf(specific);
     this.product.splice(productIndex, 1);
     }
     this.updateProduct = function(productId,newProductId,newProductName,newPrice){
-    let specificProduct = this.product.find(item => item.productId ===productId)
-    specificProduct.productId =newProductId;
-    specificProduct.productName = newProductName;
-    specificProduct.price = newPrice;
+    let theProduct = this.product.find(item => item.productId ===productId)
+    theProduct.productId =newProductId;
+    theProduct.productName = newProductName;
+    theProduct.price = newPrice;
     }
     this.getProduct = (Id)=>{
     console.log(this.product.find(object=>object.productId===Id))
@@ -43,15 +43,16 @@ class Mkulima{
     console.log(this.product)
     }
     this.calculateOrderCost = (productId,quantity)=>{
-    let specificProduct = this.product.find(object=>object.productId===productId);
-    console.log(`${quantity} ${specificProduct.productName} for Kshs ${quantity*specificProduct.price}`);
+    let theProduct = this.product.find(object=>object.productId===productId);
+    console.log(`${quantity} ${theProduct.productName} for Kshs ${quantity*theProduct.price}`);
     }
     }
     }
-    let farm1 = new Mkulima();
-    farm1.addFarm("5A 0.78ha","sharzer","icy","+254 743 123 456", "087-432");
-    farm1.addFarm("8C 0.82ha","rajan","nana","+254 754 789 101", "345-675");
-    farm1.addFarm("10B 0.25ha","tete","jackie","+254 123 456 784", "123-567")
-    console.log(farm1.farms);
-    farm1.removeFarm("10B 0.25ha");
-    console.log(farm1)
+    let farmer = new Mkulima();
+    farmer.addFarm("5A 0.78ha","sharzer","icy","+254 743 123 456", "087-432");
+    farmer.addFarm("8C 0.82ha","rajan","nana","+254 754 789 101", "345-675");
+    farmer.addFarm("10B 0.25ha","tete","jackie","+254 123 456 784", "123-567")
+    console.log(farmer.farms);
+    farmer.removeFarm("10B 0.25ha");
+    console.log(farmer)
+
